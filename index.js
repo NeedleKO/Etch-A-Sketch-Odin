c63f80dba60f60e9e4c07ab6cdd98b16720e2f6f
NeedleKO <marksjagurs@gmail.com>
2022-09-28 17:04:37 +0300

    Added 2 buttons to change color from black to random

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const containerOfGrid = document.querySelector(".container")
 const gridBtn = document.querySelector(".changeGridBtn")
+const blackBtn = document.querySelector(".blackBtn")
+const rainbowCol = document.querySelector(".rainbowCol")
 let amountOfGrids = 16;
 
+
+//test rainbow button
+const rainbowBtn = document.querySelector(".rainbowCol")
 
 
 function createGridBoxes(gridCount) {
@@ -22,20 +27,26 @@ function createGridBoxes(gridCount) {
             gridSquares.style.height = `${widthAndHeight}px` 
             gridBox.appendChild(gridSquares)
             //mouse color hover effect
-            gridSquares.addEventListener("mouseover", function() {
-                //color in black
-                //this.classList.add("gridSquaresBlack")
-
-                //random color
-                let symbols, color;
-                symbols = "0123456789ABCDEF"
-                color = "#";
-                for(let i = 0; i < 6; i++) {
-                color = color + symbols[Math.floor(Math.random() * 16)];
-                }
-                gridSquares.style.background = color;
-                //random color end
+            blackBtn.addEventListener("click", () => {
+                gridSquares.addEventListener("mouseover", function() {
+                    //color in black
+                    gridSquares.style.background = "black";
+                    //this.classList.add("gridSquaresBlack")
+                })
+            
             })
+            rainbowBtn.addEventListener("click", () => {
+                gridSquares.addEventListener("mouseover", function() {
+                    let symbols, color;
+                    symbols = "0123456789ABCDEF"
+                    color = "#";
+                    for(let i = 0; i < 6; i++) {
+                    color = color + symbols[Math.floor(Math.random() * 16)];
+                    }
+                    gridSquares.style.background = color;
+                })
+            })
+            
             wrapper.appendChild(gridBox)
         }
         containerOfGrid.appendChild(wrapper)
@@ -59,3 +70,18 @@ gridBtn.addEventListener("click", function() {
         createGridBoxes(amountOfGrids)
 
 })
+
+
+/*
+
+//random color
+                let symbols, color;
+                symbols = "0123456789ABCDEF"
+                color = "#";
+                for(let i = 0; i < 6; i++) {
+                    gridSquares.style.background = color;
+                color = color + symbols[Math.floor(Math.random() * 16)];
+                }
+                //random color end
+
+                */
